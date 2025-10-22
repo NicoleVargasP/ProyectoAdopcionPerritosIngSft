fec88f8fa7dee695b3d9635b203f7bcbfd21813b
Nicole Vargas <ariana.vargas.p@ucb.edu.bo>
2025-10-22 15:19:25 -0400

    Fix:se corrigio lso errores de las pruebas de cypress para regristro

--- src/registrarMascota.js
+++ src/registrarMascota.js
@@ -3,6 +3,12 @@ export default function registrarMascota(id, nom, edad, especie, descripcion, co
   if (!id || !nom || !edad || !especie || !descripcion || !contacto) {
     return "Por favor, completa todos los campos.";
   } else {
-    return "Se registró la mascota con éxito";
+    return "Se registró la mascota con éxito" +
+      "<p>Id: "+id+"</p>"+
+        "<p>Nombre: "+nom+"</p>"+
+        "<p>Edad: " +edad+"</p>"+
+        "<p>Especie: " +especie+"</p>"+
+        "<p>Descripcion: "+descripcion+"</p>"+
+        "<p>Contacto: "+contacto+"</p>";
   }
 }
