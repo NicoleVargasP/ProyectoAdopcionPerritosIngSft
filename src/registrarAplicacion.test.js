44ff699b38799fe90bd202372181be8e3528ea27
Andres Zubieta <andres.zubieta@ucb.edu.bo>
2025-11-25 03:31:39 -0400

    refactor: cambio en los tests para que pasen

--- src/registrarAplicacion.test.js
+++ src/registrarAplicacion.test.js
@@ -1,6 +1,6 @@
 import  registrarAplicacion  from "./registrarAplicacion";
 
-describe("Función registrarMascota", () => {
+describe("Función registrarAplicacion", () => {
 
   test("Debe devolver mensaje de error si falta algún campo", () => {
     const resultado = registrarAplicacion(8,"Pelula",null, "Jorge","jorge@email.com" , 789456, "Me Gustan los perritos", "555-123");
@@ -9,12 +9,12 @@ describe("Función registrarMascota", () => {
 
   test("Debe aceptar un número telefónico válido", () => {
     const resultado = registrarAplicacion(1, "Michi", "9876543", "Ana", "ana@mail.com", "555-123784", "Adopción");
-    expect(resultado).toContain("Se registró la aplicacion con éxito");
+    expect(resultado.mensaje).toContain("Se registró la aplicacion con éxito");
   });
 
   test("Debe aceptar un correo electrónico válido", () => {
     const resultado = registrarAplicacion(2, "Toby", "6543210", "Carlos", "carlos@mail.com", "+59176543210", "Adopción");
-    expect(resultado).toContain("Se registró la aplicacion con éxito");
+    expect(resultado.mensaje).toContain("Se registró la aplicacion con éxito");
   });
 
   test("Debe devolver error si el correo y número son inválidos", () => {
